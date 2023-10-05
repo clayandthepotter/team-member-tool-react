6be8587ff8df18ee26bb85e651dcf70fec0acd8d
Clayton Prickett <clayton.a.prickett@gmail.com>
2023-10-04 19:02:28 -0500

    commit

--- src/GroupedTeamMembers.jsx
+++ src/GroupedTeamMembers.jsx
@@ -3,15 +3,77 @@ import { useState } from "react";
 
 const GroupedTeamMembers = ({employees, selectedTeam, setTeam}) => {
 
-    const [groupedEmployees, setGroupedData] = useState(groupedTeamMembers());
+    const [groupedEmployees, setGroupedData] = useState(groupTeamMembers());
+
+    function groupTeamMembers() {
+
+      let teams = [];
+      
+      const teamAMembers = employees.filter(
+        (employee) => employee.teamName === 'TeamA'
+      );
+      const teamA = {team: 'TeamA', members:teamAMembers, collapsed: selectedTeam === 'TeamA' ? false : true}
+
+      const teamBMembers = employees.filter(
+				(employee) => employee.teamName === 'TeamB'
+			);
+			const TeamB = {
+				team: 'TeamB',
+				members: teamBMembers,
+				collapsed: selectedTeam === 'TeamB' ? false : true,
+			};
+
+      const teamCMembers = employees.filter(
+				(employee) => employee.teamName === 'TeamC'
+			);
+			const TeamC = {
+				team: 'TeamC',
+				members: teamCMembers,
+				collapsed: selectedTeam === 'TeamC' ? false : true,
+			};
+
+      const teamDMembers = employees.filter(
+				(employee) => employee.teamName === 'TeamD'
+			);
+			const TeamD = {
+				team: 'TeamD',
+				members: teamDMembers,
+				collapsed: selectedTeam === 'TeamD' ? false : true,
+			};
+    }
+
+    function handleTeamClick(event) {
+      const newGroupData = groupedEmployees.map((groupedData) => groupedData.team === event.currentTarget.id ? {...groupedData, collaped:!groupedData.collaped});
+    }
+
 
     return (
       <main className="container">
-        <div className="row justify-content-center mt-3 mb-4">
-          <div className="col-8">
-            <h1>Grouped Team Members</h1>
-          </div>
-        </div>
+        {
+          groupedEmployees.map((item) => {
+            return (
+              <div key={item.team} className="card mt-2" style={{cursor:"pointer"}}>
+                <h4 id={item.team} className="card-header text-secondary bg-white" onClick={handleTeamClick}>{item.team}
+                </h4>
+                <div id={"collapse_" + item.team} className={item.collapsed === true?"collapsed":""}>
+                  <hr />
+                  {
+                    item.members.map(member => {
+                      return (
+                        <div className="mt-2">
+                          <h5 className="card-title mt-2">
+                            <span className="text-dark">Full Name: {member.fullName}</span>
+                          </h5>
+                          <p>Designation: {member.designation}</p>
+                        </div>
+                      )
+                    })
+                  }
+                </div>
+              </div>
+            )
+          })
+        }
       </main>
     )
   }
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,7 +4,7 @@ const Nav = () => {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <ul className="navar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
                     <Link className="nav-link" to ='/'>Home</Link>
                 </li>
